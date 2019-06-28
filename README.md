fa3347e91ea76ab6c75373349a9c0b5d945e95fc
Ankur Srivastava <ankursrivastav35@gmail.com>
2019-06-28 11:18:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+### **gomon** - tcp based client server for running remote commands
+
+`gomon` uses tcp socket to start and server. Any no of `gomon` client can connect to the server and request for running commands.
+
+### Usage
+---
+```
+➜  ~ gomon
+usage: gomon <command> [<args>]
+The most common gmon commands are
+    serve    start the gomon server
+  connect    connect to the gomon server
+```
+#### Starting the server
+The server accepts a command line flag to run on a custom port. For details see `gomon serve -h`
+```
+gomon serve [-port=port]
+```
+
+#### Connecting to `gomon` server and running command
+```
+gomon connect -remotePort <port> -remoteHost <host/ip> -command <command>
+```
+
+### Examples
+```
+➜  ~ gomon serve
+2019/06/28 11:12:16 server listening on [::]:7891
+
+➜  ~ gomon connect -remoteHost localhost -remotePort 7891 -command 'uname -a'
+2019/06/28 11:13:02 Connected to 127.0.0.1:7891
+Linux Vostro-15-3568 4.15.0-52-generic #56-Ubuntu SMP Tue Jun 4 22:49:08 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
+
+```
+
+### How to build and install
+-   Download or clone this [repository](https://github.com/srvstva/gomon)
+-   `cd gomon && go build && go install`
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"encoding/gob"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// Request type
+type Request struct {
+	Action  string
+	Command string
+}
+
+// Response type
+type Response struct {
+	Error  error
+	Result []byte
+}
+
+func encodeRequest(w io.Writer, r Request) {
+	encoder := gob.NewEncoder(w)
+	encoder.Encode(r)
+}
+
+func decodeRequest(r io.Reader) Request {
+	req := Request{}
+	decoder := gob.NewDecoder(r)
+	decoder.Decode(&req)
+	return req
+}
+
+func encodeResponse(w io.Writer, r Response) {
+	encoder := gob.NewEncoder(w)
+	encoder.Encode(r)
+}
+
+func decodeResponse(r io.Reader) Response {
+	resp := Response{}
+	decoder := gob.NewDecoder(r)
+	decoder.Decode(&resp)
+	return resp
+}
+
+var (
+	serverCommand = flag.NewFlagSet("serve", flag.ExitOnError)
+	serverPort    = serverCommand.String("port", ":7891", "port to listen on")
+	clientCommand = flag.NewFlagSet("connect", flag.ExitOnError)
+	remoteHost    = clientCommand.String("remoteHost", "localhost", "hostname/ip to connect to")
+	remotePort    = clientCommand.String("remotePort", ":7891", "remote port to connect to")
+	command       = clientCommand.String("command", "", "command to run at server")
+)
+
+func init() {
+	if len(os.Args) == 1 {
+		fmt.Println("usage: gomon <command> [<args>]")
+		fmt.Println("The most common gmon commands are")
+		fmt.Println("    serve    start the gomon server")
+		fmt.Println("  connect    connect to the gomon server")
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "serve":
+		serverCommand.Parse(os.Args[2:])
+	case "connect":
+		clientCommand.Parse(os.Args[2:])
+	}
+}
+
+func main() {
+	if serverCommand.Parsed() {
+		startServer(*serverPort)
+
+	} else if clientCommand.Parsed() {
+		if *command == "" {
+			fmt.Println("-command requires an argument")
+			os.Exit(2)
+		}
+		startClient(*remoteHost, *remotePort, *command)
+	}
+}
+
+func startServer(port string) {
+	if !strings.HasPrefix(port, ":") {
+		port = ":" + port
+	}
+	l, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer l.Close()
+	log.Printf("server listening on %s\n", l.Addr().String())
+	for {
+		c, _ := l.Accept()
+		go func(c net.Conn) {
+			req := decodeRequest(c)
+			switch req.Action {
+			case "run":
+				output, err := exec.Command("bash", "-c", req.Command).CombinedOutput()
+				resp := Response{Error: err, Result: output}
+				encodeResponse(c, resp)
+				log.Printf("%s [%s]\n", c.RemoteAddr().String(), req.Command)
+				c.Close()
+			default:
+				c.Write([]byte("invalid action\n"))
+				c.Close()
+			}
+		}(c)
+	}
+}
+
+func startClient(host, port, command string) {
+	if !strings.HasPrefix(port, ":") {
+		port = ":" + port
+	}
+	c, err := net.Dial("tcp", host+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+	log.Printf("Connected to %s\n", c.RemoteAddr().String())
+	req := Request{Action: "run", Command: command}
+	encodeRequest(c, req)
+	resp := decodeResponse(c)
+	if resp.Error != nil {
+		log.Println("error running command", resp.Error)
+	}
+	fmt.Print(string(resp.Result))
+}
